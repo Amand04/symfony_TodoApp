2aef06016900b131093ca2044fce62282ba1dc43
Amandine <amandinejeanjules@free.fr>
2022-04-21 11:02:49 +0200

    initialisation projet, création entités et base de données

---
+++ src/Entity/Task.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=90)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $completed;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=TodoList::class, inversedBy="tasks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+
+    private $list;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of title
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of completed
+     */
+    public function getCompleted(): ?bool
+    {
+        return $this->completed;
+    }
+
+    /**
+     * Set the value of completed
+     *
+     * @return  self
+     */
+    public function setCompleted(bool $completed)
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of list
+     */
+    public function getList(): ?TodoList
+    {
+        return $this->list;
+    }
+
+    /**
+     * Set the value of list
+     *
+     * @return  self
+     */
+    public function setList(?TodoList $list)
+    {
+        $this->list = $list;
+
+        return $this;
+    }
+}
