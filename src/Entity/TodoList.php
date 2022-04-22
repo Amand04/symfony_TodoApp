85a8c6b6a1750a3d74b36ba5606c9e396fb8c521
Amandine <amandinejeanjules@free.fr>
2022-04-22 13:15:50 +0200

    create, update, read

--- src/Entity/TodoList.php
+++ src/Entity/TodoList.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -47,11 +48,11 @@ class TodoList
      */
     private $tasks;
 
-    /*public function __construct()
+    public function __construct()
     {
-        $this->tasks = new ArrayCollection();
+        $this->Tasks = new ArrayCollection();
     }
-    */
+
     /**
      * Get the value of id
      */
